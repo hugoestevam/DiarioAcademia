e61373e4f5c2a20f8b76ddf814a9482d6b38087c
alexandre.rech <alexandre.rech@hotmail.com>
2014-11-28 17:43:29 -0200

    Adicionado AulaForms

---
+++ NDDigital.DiarioAcademia.Apresentacao.WindowsApp/Controls/AulaForms/AulaDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NDDigital.DiarioAcademia.Apresentacao.WindowsApp.Controls.AulaForms
+{
+    public partial class AulaDialog : Form
+    {
+        public AulaDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
