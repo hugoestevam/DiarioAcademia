43f6bd4d583f1d0fc4bb978803a53375781695e4
thiagosartor <tt.sartor@hotmail.com>
2015-07-17 14:24:48 -0300

    Ajustando as classes que não foram para o git.

---
+++ NDDigital.DiarioAcademia.Infraestrutura.Orm/Contexts/AuthContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDigital.DiarioAcademia.Infraestrutura.Orm.Contexts
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("DiarioAuthContext")
+        {
+
+        }
+    }
+}
