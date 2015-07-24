57f4130ee5aa3ad350ca4593bc3122173f8d977c
thiagosartor <tt.sartor@hotmail.com>
2015-07-24 15:52:14 -0300

    Implementando a parte de autenticação na Web Api.

---
+++ NDDigital.DiarioAcademia.Infraestrutura.WebServices/EmailService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDigital.DiarioAcademia.Infraestrutura.WebServices
+{
+    internal class MailInfo
+    {
+
+        internal string Mails_Host_Server;
+        internal string Sender_Mail;
+        internal string Sender_Mail_Name;
+        internal string Login_User;
+        internal string Login_Pass;
+        internal string DefaultReceiver;
+        internal int SmtpServerPor;
+
+
+
+    }
+    public class EmailService : IIdentityMessageService
+    {
+        private MailInfo MailInfo;
+        public EmailService()
+        {
+            MailInfo = new MailInfo
+            {
+                Login_User = "nddprint",
+                Mails_Host_Server = "mail.nddigital.com.br",
+                Sender_Mail = "nddprint@nddigital.com.br",
+                Sender_Mail_Name = "Portal nddprint",
+                Login_Pass = "vU72x@#oL",
+                SmtpServerPor = 25,
+
+                DefaultReceiver = "wesley.lemos@nddigital.com.br"
+            };
+        }
+
+
+        public async Task<bool> Send(string to, string subject, string body, MemoryStream anexo = null)
+        {
+            bool ret = true;
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    using (SmtpClient SmtpServer = new SmtpClient(MailInfo.Mails_Host_Server))
+                    {
+                        try
+                        {
+                            mail.From = new MailAddress(MailInfo.Sender_Mail, MailInfo.Sender_Mail_Name,
+                                Encoding.Default);
+                            mail.To.Add(to);
+                            mail.Subject = subject;
+
+                            mail.IsBodyHtml = true;
+                            mail.Body = body;
+
+                            if (anexo != null)
+                            {
+                                mail.Attachments.Add(new Attachment(anexo, subject + ".pdf"));
+
+                            }
+
+                            SmtpServer.Port = MailInfo.SmtpServerPor;
+                            SmtpServer.Credentials = new System.Net.NetworkCredential(MailInfo.Login_User, MailInfo.Login_Pass);
+                            SmtpServer.EnableSsl = true;
+
+                            SmtpServer.Send(mail);
+                        }
+                        catch (Exception exe)
+                        {
+                            for (; exe.InnerException != null;)
+                            {
+                                exe = exe.InnerException;
+
+                                Console.WriteLine(String.Format("Error: " + exe.Message));
+                            }
+                            ret = false;
+                        }
+                    }
+                }
+            }
+            catch (Exception exe)
+            {
+                for (; exe.InnerException != null;)
+                {
+                    exe = exe.InnerException;
+
+                    Console.WriteLine(String.Format("Error: " + exe.Message));
+                }
+                ret = false;
+            }
+
+            return ret;
+        }
+
+
+        public Task SendAsync(IdentityMessage message)
+        {
+            return Send(message.Destination, MailInfo.Login_User, message.Body);
+        }
+    }
+}
--- NDDigital.DiarioAcademia.WebApi/Startup.cs
+++ NDDigital.DiarioAcademia.WebApi/Startup.cs
@@ -8,6 +8,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using NDDigital.DiarioAcademia.Infraestrutura.Orm.Identity;
+using Microsoft.Owin.Security.OAuth;
+using NDDigital.DiarioAcademia.WebApi.Providers;
+using System.Configuration;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
 
 [assembly: OwinStartup(typeof(NDDigital.DiarioAcademia.WebApi.Startup))]
 namespace NDDigital.DiarioAcademia.WebApi
@@ -18,18 +27,61 @@ namespace NDDigital.DiarioAcademia.WebApi
         {
             var config = new HttpConfiguration();
 
-            app.UseCors(CorsOptions.AllowAll);
-
-            app.UseWebApi(config);
-
             OAuthConfig.ConfigureOAuth(app);
 
             RoutesConfig.Register(config);
 
             FormattersConfig.Configure(config);
 
-            
+            ConfigureOAuthTokenGeneration(app);
+
+            ConfigureOAuthTokenConsumption(app);
+
+            app.UseCors(CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
         }
- 
+
+        private void ConfigureOAuthTokenGeneration(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:31648")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+        }
+
+        private void ConfigureOAuthTokenConsumption(IAppBuilder app)
+        {
+
+            var issuer = "http://localhost:31648";
+            string audienceId = ConfigurationManager.AppSettings["as:AudienceId"];
+            byte[] audienceSecret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["as:AudienceSecret"]);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audienceId },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, audienceSecret)
+                    }
+                });
+        }
+
     }
 }
