504f93aad2c06a408f4dd6abc5f4ab29edb434c6
Wesley Lemos <anisan_wesley@live.com>
2015-08-19 09:26:28 -0300

    Projeto Extensions

---
+++ Infraestrutura.Extensions/ListExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Extensions
+{
+    public static class ListExtension
+    {
+        public static void AddIfNot(this List<object> list, List<object> others)
+        {
+            foreach (var item in others)
+            {
+                if (!list.Contains(item))
+                {
+                    list.Add(item);
+                }
+            }
+        }
+        public static void AddIfNot(this List<object> list, object item)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+            }
+
+        }
+    }
+}
