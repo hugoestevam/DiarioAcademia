c88c1428ced32ccb688128e9d387a3f9cf8f4341
hugoestevam <hugoestevamrl@hotmail.com>
2015-10-21 15:32:23 -0200

    Alterando os testes de IoC para testarem somente a injeção de dependencia #Part2

--- NDDigital.DiarioAcademia.IntegrationTests/IoC/IoCNinjectSqlTest.cs
+++ NDDigital.DiarioAcademia.IntegrationTests/IoC/IoCNinjectSqlTest.cs
@@ -2,6 +2,7 @@
 using NDDigital.DiarioAcademia.Dominio.Contracts;
 using NDDigital.DiarioAcademia.Dominio.Entities;
 using NDDigital.DiarioAcademia.Infraestrutura.IoC;
+using NDDigital.DiarioAcademia.Infraestrutura.SQL.Repositories;
 using NDDigital.DiarioAcademia.IntegrationTests.Base;
 using System.Configuration;
 
